eca4b34dea18d32389365171bd68ed9c70bf7b05
CoralieGold <coralie.gold@gmail.com>
2018-02-14 20:42:49 +0100

    Add virtualenv

--- StoneEdgeGeneration/UI/TerrainWindow.py
+++ StoneEdgeGeneration/UI/TerrainWindow.py
@@ -1,17 +1,11 @@
-import sys 
-import bpy
 from importlib import reload
 
-from PyQt5.QtCore import Qt
-from PyQt5 import QtGui, QtCore
-from PyQt5 import QtWidgets
-
 from StoneEdgeGeneration import utils, UI
-from StoneEdgeGeneration.UI import BaseWindow, Parameter
+from StoneEdgeGeneration.UI import BaseWindow
 reload(utils)
 reload(UI)
-reload(Parameter)
 reload(BaseWindow)
+
 
 class TerrainWindow(BaseWindow.BaseWindow):
     def __init__(self):
