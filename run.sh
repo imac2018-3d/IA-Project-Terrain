0edf6196254c99cb3270890cc3e3592bf1de19b3
hellowizz <heloroussel@hotmail.fr>
2018-03-08 14:30:41 +0100

    recup

--- run.sh
+++ run.sh
@@ -10,15 +10,15 @@ python_path="${blender_path}/2.79/python/bin"
 echo "Python executable: ${python_path}/python"
 echo "change directory to StoneEdgeGeneration"
 cd StoneEdgeGeneration
-eval "${python_path}/python -m pip install virtualenv"
+eval "pip install virtualenv"
 
 if [ ! -d "virtualenv" ]; then
-  eval "${python_path}/python -m virtualenv virtualenv"
+  eval "virtualenv virtualenv"
 fi
 echo "change directory to virtualenv"
 cd virtualenv
 source Scripts/activate
-eval "${python_path}/python -m pip install -r requirements.txt"
+eval "pip install -r requirements.txt"
 echo "change directory to ${root}"
 cd $root
 eval "${blender_path}/blender main.blend"
